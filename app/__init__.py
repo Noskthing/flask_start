e05518dbeebc02d4202899e6fd60890cd21b83cc
Mansk <leeB0Wen@163.com>
2017-01-05 20:09:03 +0800

    no message

--- app/__init__.py
+++ app/__init__.py
@@ -33,10 +33,10 @@ def create_app(config_name):
     pagedown.init_app(app)
 
     db.init_app(app)
-    # with app.app_context():
-    #     # Extensions like Flask-SQLAlchemy now know what the "current" app
-    #     # is while within this block. Therefore, you can now run........
-    #     db.create_all()
+    with app.app_context():
+        # Extensions like Flask-SQLAlchemy now know what the "current" app
+        # is while within this block. Therefore, you can now run........
+        db.create_all()
         
     login_manager.init_app(app)
 
