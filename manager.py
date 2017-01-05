411d93296d6f91f27367c3a55d9e685d3437e047
Mansk <leeB0Wen@163.com>
2017-01-05 20:01:59 +0800

    no message

--- manager.py
+++ manager.py
@@ -28,8 +28,8 @@ def apprun():
 
 
 if __name__ == '__main__':
-	# with app.app_context():
- #        # Extensions like Flask-SQLAlchemy now know what the "current" app
- #        # is while within this block. Therefore, you can now run........
-	# 	db.create_all()
+	with app.app_context():
+        # Extensions like Flask-SQLAlchemy now know what the "current" app
+        # is while within this block. Therefore, you can now run........
+		db.create_all()
 	app.run()
