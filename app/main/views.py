eb7bf6b720799afb9fb1ec89e91b07945afb51a9
Mansk <leeB0Wen@163.com>
2017-01-10 20:21:39 +0800

    aaa

--- app/main/views.py
+++ app/main/views.py
@@ -11,16 +11,16 @@ from ..models import Role, User, Permission, Post, Comment
 from ..decorators import admin_required, permission_required
 from .. import db
 
-@main.after_app_request
-def after_request(response):
-    for query in get_debug_queries():
-        if query.duration >= current_app.config['FLASKY_SLOW_DB_QUERY_TIME']:
-            current_app.logger.warning(
-                'Slow query: %s\nParameters: %s\nDuration: %fs\nContext: %s\n'
-                % (query.statement, query.parameters, query.duration,
-                   query.context)
-                )
-        return response
+# @main.after_app_request
+# def after_request(response):
+#     for query in get_debug_queries():
+#         if query.duration >= current_app.config['FLASKY_SLOW_DB_QUERY_TIME']:
+#             current_app.logger.warning(
+#                 'Slow query: %s\nParameters: %s\nDuration: %fs\nContext: %s\n'
+#                 % (query.statement, query.parameters, query.duration,
+#                    query.context)
+#                 )
+#         return response
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
